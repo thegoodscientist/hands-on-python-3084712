72cdcad6411b61c19d06ca9975a1a2d55d2990cb
thegoodscientist <70919341+thegoodscientist@users.noreply.github.com>
2022-11-25 18:08:19 +0000

    Part 2 changes

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -1,12 +1,16 @@
 greet = "Hello World"
-extened_grt = "Hello World, " + "this is a long string"
+extended_greet = "Hello World, " + "this is a long string"
 
 name = "John"
 
-intrupution = f"Hello {name}"
+interpolation = f"Hello {name}"
 
 greet_format = "Hello {}"
 
 formatted = greet_format.format(name)
 
-print(intrupution, formatted)
+print(interpolation, formatted)
+
+print(formatted.upper())
+print(formatted.lower())
+print(formatted.replace("John", "Paul"))
